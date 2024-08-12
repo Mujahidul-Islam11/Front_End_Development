5864be272cee7c7e5bb87f3403695e83acac7ef6
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-08-12 11:29:26 +0600

    Loop concept completed

---
+++ JavaScript/Level one/script.js
@@ -0,0 +1,24 @@
+// for Loop -> kono kichu infinite ba nirdisto time porjonto repeat kora.
+
+for (var i = 0; i < 10; i++) {
+  console.log("hello" + i);
+}
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+// for and while loop duitai eki kaj kore sudhu likhar dhoron alada
+
+
+// dhori amra ekta loop jeitar condition sotto nah tawo amra ekbar loop ta calaite cai, ekhon for
+// loop a emon condition dilam jeta sotto nah, ekhon loop ta ki colbe ekbar o? nah colbe nah, jodi
+// cai condition sotto nah howar por o ekbar coluk tobe do-while loop calate hobe
+
+var i = 0;
+do{
+    console.log("Hello" + "" + i);
+    i++;
+}while(i<0)
