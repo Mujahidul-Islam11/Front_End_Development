7f1e55d6ccadbbdd2096d7571f0e63e40363f4d8
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-08-13 11:17:43 +0600

    var const let scope

--- JavaScript/Level one/script.js
+++ JavaScript/Level one/script.js
@@ -1,35 +1,34 @@
 // for Loop -> kono kichu infinite ba nirdisto time porjonto repeat kora.
 
-for (var i = 0; i < 10; i++) {
-  console.log("hello" + i);
-}
+// for (var i = 0; i < 10; i++) {
+//   console.log("hello" + i);
+// }
 
-var i = 0;
-while (i < 10) {
-  console.log(i);
-  i++;
-}
+// var i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
 
 // for and while loop duitai eki kaj kore sudhu likhar dhoron alada
-
 
 // dhori amra ekta loop jeitar condition sotto nah tawo amra ekbar loop ta calaite cai, ekhon for
 // loop a emon condition dilam jeta sotto nah, ekhon loop ta ki colbe ekbar o? nah colbe nah, jodi
 // cai condition sotto nah howar por o ekbar coluk tobe do-while loop calate hobe
 
-var i = 0;
-do{
-    console.log("Hello" + "" + i);
-    i++;
-}while(i<0)
+// var i = 0;
+// do{
+//     console.log("Hello" + "" + i);
+//     i++;
+// }while(i<0)
 
-{
-    const name = "dinar";
-    let age = 18;
-}
-
-console.log(name);
-console.log(age);
+// {
+//     const name = "dinar";
+//     let age = 18;
+// }
+// console.log(name);
+// console.log(age);
 // const and let braces scope er hoye thake, orthat const ar let er age pore braces lagiye dile
 // setai tar scope hoye jai braces{} er baire take access kora jabe nah, kintu bhitore jeikono jaigai
 // kora jabe
+
